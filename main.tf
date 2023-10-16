bc81d45b2b31e221430fa2c1a493c64add54f945
Pallavi Mishra <39494043+pallavim98@users.noreply.github.com>
2023-10-15 20:40:10 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = "nginx"  # Use the image name here
+  name  = "tutorial"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
